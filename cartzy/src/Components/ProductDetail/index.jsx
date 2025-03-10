88b5bf2557b58ea362bc8235e178aff6fdeaa710
David <luisdavidbeslanga@gmail.com>
2025-03-10 15:16:53 -0500

    cambios tienda

--- cartzy/src/Components/ProductDetail/index.jsx
+++ cartzy/src/Components/ProductDetail/index.jsx
@@ -4,7 +4,7 @@ const ProductDetail = ({ data, onClose }) => {
   if (!data) return null; // 👈 Evita renderizar si no hay producto seleccionado
 
   return (
-    <aside className="w-[360px] flex flex-col fixed bg-white right-0 border border-black rounded-lg shadow-lg p-4">
+    <aside className="w-[360px] z-50 flex flex-col fixed bg-white right-0 border border-black rounded-lg shadow-lg p-4">
       <button onClick={onClose} className="self-end text-lg font-bold">✖</button> {/* Botón para cerrar */}
       <img src={data.images?.[0]} alt={data.title} className="object-cover w-full h-40 rounded-lg" />
       <h2 className="mt-2 text-lg font-bold">{data.title}</h2>
